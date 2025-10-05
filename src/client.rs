9c9f5ab4a943f9cbfe1e09dfc1ba725cb836c403
Alexandre Maurel <alexandre.maurel@gmail.com>
2025-10-05 14:26:57 +0200

    endpoint address

--- src/client.rs
+++ src/client.rs
@@ -50,13 +50,13 @@ impl From<serde_json::Error> for Error {
 // Private data
 
 static ENDPOINTS: phf::Map<&'static str, &'static str> = phf::phf_map! {
-    "ovh-eu" => "https://eu.api.ovh.com/1.0",
-    "ovh-us" => "https://api.us.ovhcloud.com/1.0",
-    "ovh-ca" => "https://ca.api.ovh.com/1.0",
-    "kimsufi-eu" => "https://eu.api.kimsufi.com/1.0",
-    "kimsufi-ca" => "https://ca.api.kimsufi.com/1.0",
-    "soyoustart-eu" => "https://eu.api.soyoustart.com/1.0",
-    "soyoustart-ca" => "https://ca.api.soyoustart.com/1.0",
+    "ovh-eu" => "https://eu.api.ovh.com/v1",
+    "ovh-us" => "https://api.us.ovhcloud.com/v1",
+    "ovh-ca" => "https://ca.api.ovh.com/v1",
+    "kimsufi-eu" => "https://eu.api.kimsufi.com/v1",
+    "kimsufi-ca" => "https://ca.api.kimsufi.com/v1",
+    "soyoustart-eu" => "https://eu.api.soyoustart.com/v1",
+    "soyoustart-ca" => "https://ca.api.soyoustart.com/v1",
 };
 
 // Private helpers
